6080ba4ed46bdea8d47183918fd6649dc4cee11a
Matt <matt-paul@live.com>
2020-12-21 20:49:47 -0500

    completed the details page, finished pt2 of lab

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
    path('', views.home, name='home'), 
    path('about/', views.about, name='about'),
-    path('coins/', views.coins_index, name='index'),
+   path('coins/', views.coins_index, name='index'),
+   path('coins/<int:coin_id>/', views.coins_detail, name='detail'),
 ]
