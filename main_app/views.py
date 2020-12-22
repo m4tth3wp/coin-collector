c947dc31ff998dcea9bfd9faa458c7f2b8492897
Matt <matt-paul@live.com>
2020-12-21 20:18:51 -0500

    Updates: -added the coin model -added a home page that renders -removed the dummy data

--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Coin
 
 # Create your views here.
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html') 
 
 def coins_index(request):
+    coins = Coin.objects.all()
     return render(request, 'coins/index.html', {'coins': coins})
-
-# Add the Cat class & list and view function below the imports
-class Coin:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, currency, description, value):
-    self.name = name
-    self.currency = currency
-    self.description = description
-    self.value = value
-
-coins = [
-  Coin('Canadian Dollar', 'CAD', 'founded in Canada', 0.78),
-  Coin('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Coin('Raven', 'black tripod', '3 legged cat', 4)
-]
