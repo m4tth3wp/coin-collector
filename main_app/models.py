eee959aa10a7bbc5b568a51663c1d1be64063e6f
Matt <matt-paul@live.com>
2020-12-27 21:35:38 -0500

    added the acquired one-to-many model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.urls import reverse
 
+ANSWER = (
+  ('Y', 'Yes'),
+  ('N', 'No')
+)
+
 # Create your models here.
 class Coin(models.Model):
   name = models.CharField(max_length=100)
@@ -13,3 +18,17 @@ class Coin(models.Model):
   
   def get_absolute_url(self):
     return reverse('detail', kwargs={'coin_id': self.id})
+  
+class Acquired(models.Model):
+    owned = models.CharField(
+      max_length=100,
+      choices=ANSWER,
+      default=ANSWER[0][1]
+    )
+    date = models.DateField()
+
+    coin = models.ForeignKey(Coin, on_delete=models.CASCADE)
+
+    def __str__(self):
+      return f'{self.get_owned_display()} on {self.date}'
+  
